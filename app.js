d36eaf89f9dd98e2c434529855b2cb25cda0342a
Jani Muhlestein <janimuhlestein@gmail.com>
2020-09-20 04:16:08 -0600

    created webpage

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
 //console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
+/*const printProfileData = profileDataArr => {
    for(let i = 0; i<profileDataArr.length; i +=1) {
        console.log(profileDataArr[i]);
        console.log('=================');
@@ -9,3 +13,9 @@ const printProfileData = profileDataArr => {
    }
 };
     printProfileData(profileDataArgs);
+*/
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
