da066474bdf0b3c068685a550406e9b0f4bdeb08
Jani Muhlestein <janimuhlestein@gmail.com>
2020-09-20 15:56:55 -0600

    Added inquirer code

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+/*const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
-//console.log(profileDataArgs);
-
-/*const printProfileData = profileDataArr => {
-   for(let i = 0; i<profileDataArr.length; i +=1) {
-       console.log(profileDataArr[i]);
-       console.log('=================');
-      profileDataArr.forEach(profileItem => console.log(profileItem));
-   }
-};
-    printProfileData(profileDataArgs);
-*/
 
 fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
